01435b0d6f343ff0e03fddb49e23088909bec447
David Yang <daworld.ny@gmail.com>
2016-08-01 22:28:31 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'name'  => 'Menu',
-        'route' => 'Menu:Admin:Item@indexAction',
-        'icon'  => 'fa fa-bars fa-5x',
-        'order' => 3,
-        'description' => 'Menu modules allows you to easily handle different menus on your site'
-    )
+ 'name'  => 'Menu',
+ 'route' => 'Menu:Admin:Item@indexAction',
+ 'icon'  => 'fa fa-bars fa-5x',
+ 'order' => 3,
+ 'description' => 'Menu modules allows you to easily handle different menus on your site'
 );
