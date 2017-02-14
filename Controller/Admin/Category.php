0732ceaf3264654d02f4d84f581ad0945c1bb99e
David Yang <daworld.ny@gmail.com>
2017-02-14 23:33:48 +0500

    Dropped extra abstraction

--- Controller/Admin/Category.php
+++ Controller/Admin/Category.php
@@ -72,7 +72,11 @@ final class Category extends AbstractAdminController
      */
     public function deleteAction($id)
     {
-        return $this->invokeRemoval('categoryManager', $id);
+        $service = $this->getModuleService('categoryManager');
+        $service->deleteById($id);
+
+        $this->flashBag->set('success', 'Selected element has been removed successfully');
+        return '1';
     }
 
     /**
@@ -84,7 +88,7 @@ final class Category extends AbstractAdminController
     {
         $input = $this->request->getPost('category');
 
-        return $this->invokeSave('categoryManager', $input['id'], $input, array(
+        $formValidator = $this->createValidator(array(
             'input' => array(
                 'source' => $input,
                 'definition' => array(
@@ -93,5 +97,25 @@ final class Category extends AbstractAdminController
                 )
             )
         ));
+
+        if ($formValidator->isValid()) {
+            $service = $this->getModuleService('categoryManager');
+
+            if (!empty($input['id'])) {
+                if ($service->update($input)) {
+                    $this->flashBag->set('success', 'The element has been updated successfully');
+                    return '1';
+                }
+
+            } else {
+                if ($service->add($input)) {
+                    $this->flashBag->set('success', 'The element has been created successfully');
+                    return $service->getLastId();
+                }
+            }
+
+        } else {
+            return $formValidator->getErrors();
+        }
     }
 }
